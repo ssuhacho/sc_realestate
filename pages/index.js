7535300208676bdbbcee03cc8e6e9668aa49e2a6
ssuhacho <ssuhacho@gmail.com>
2022-03-01 13:49:42 -0600

    fourth commit

--- pages/index.js
+++ pages/index.js
@@ -21,10 +21,8 @@ const Banner = ({purpose, title1, title2, desc1, desc2, buttonText, linkName, im
 )
 
 export default function Home({propertiesForRent,propertiesForSale}) {
-  console.log(propertiesForSale, propertiesForRent);
   return (
     <Box>
-      <h1>Hello World!</h1>
       <Banner 
       purpose = "RENT A HOME"
       title1 = "Rental Homes For"
@@ -48,7 +46,9 @@ export default function Home({propertiesForRent,propertiesForSale}) {
       linkName ="/search?purpose=for-sale"
       imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'  
       />
+      <Flex flexWrap="wrap">
         {propertiesForSale.map((property)=> <Property property={property} key={property.id} />)}
+        </Flex>
     </Box>
   )
 }
