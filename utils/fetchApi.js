9769e6e9dd0d28d19f8c7e74567dbb6e8f81ea62
ssuhacho <ssuhacho@gmail.com>
2022-03-18 05:56:26 -0500

    first commit

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,8 +6,8 @@ export const fetchApi = async (url) => {
     const {data} = await axios.get((url), {
         headers: {
             'x-rapidapi-host': 'bayut.p.rapidapi.com',
-            'x-rapidapi-key': '4638f8ee13msh5ea00fc54fe53d6p1fb6d4jsnbec91c20febe'
-          }
+            'x-rapidapi-key': '4638f8ee13msh5ea00fc54fe53d6p1fb6d4jsnbec91c20febe' ,
+          },
     });
     return data;
 }
